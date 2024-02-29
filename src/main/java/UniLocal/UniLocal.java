343d025c07b376021f0453dd3ea4e1431db0dfe9
JoanGomez <joangomezusma4@gmail.com>
2024-02-29 09:44:44 -0500

    Inicio del proyecto Unilocal

---
+++ src/main/java/UniLocal/UniLocal.java
@@ -0,0 +1,9 @@
+package UniLocal;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class UniLocal {
+    public static void main(String[] args) {
+        SpringApplication.run(UniLocal.class, args);
+    }
+}
